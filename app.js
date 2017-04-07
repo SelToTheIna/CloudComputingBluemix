54b76c2fc8f3dff49dad08777e14bd237c97ac04
Selina Magnin <selina.magnin@web.de>
2017-04-07 16:07:56 +0200

    changed device

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 //new
 var appClientConfig = {
     'org' : 'y9ckd8',
-    'id' : 'vinc',
+    'id' : 'oxischmocksie',
     'domain': 'internetofthings.ibmcloud.com',
     'auth-key' : 'a-y9ckd8-n7t8a5qjcs',
     'auth-token' : 'D3u+aQYs(G)gviDM?!'
@@ -32,7 +32,7 @@ console.log('***************Connecting')
     appClient.subscribeToDeviceEvents();
 });
 appClient.on("deviceEvent", function (deviceType, deviceId, eventType, format, payload) {
-
+//Device Event from :: iot-phone : vinc of event sensorData with payload : {"d":{"id":"vinc","ts":1491571825251,"ax":-0.64,"ay":-0.56,"az":0.68,"oa":104.95,"ob":16.7,"og":-50.51}}
     console.log("Device Event from :: "+deviceType+" : "+deviceId+" of event "+eventType+" with payload : "+payload);
 });
     //new end
